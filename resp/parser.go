d909b509013552c4ff4399211ed0a152671624ee
Martin Angers <martin.n.angers@gmail.com>
2014-05-02 14:38:09 -0400

    add Integer parsing

--- resp/parser.go
+++ resp/parser.go
@@ -2,7 +2,12 @@ package resp
 
 import "errors"
 
-var ErrMissingCRLF = errors.New("resp: missing CRLF")
+var (
+	ErrInvalidInteger = errors.New("resp: invalid integer character")
+	ErrMissingCRLF    = errors.New("resp: missing CRLF")
+)
+
+type Integer int64
 
 type Error []byte
 
@@ -17,27 +22,69 @@ func Decode(b []byte) ([]interface{}, error) {
 }
 
 func decodePayload(b []byte, isreq bool) (interface{}, error) {
+	var val interface{}
+	var err error
+
 	for i := 0; i < len(b); {
 		ch := b[i]
 		i++
 		switch ch {
 		case '+':
 			// Simple string
-			val, _, err := decodeSimpleString(b[i:])
-			if err != nil {
-				return nil, err
-			}
-			return val, nil
+			val, _, err = decodeSimpleString(b[i:])
 		case '-':
 			// Error
-			val, _, err := decodeError(b[i:])
-			if err != nil {
-				return nil, err
-			}
-			return val, nil
+			val, _, err = decodeError(b[i:])
+		case ':':
+			// Integer
+			val, _, err = decodeInteger(b[i:])
 		}
+		if err != nil {
+			return nil, err
+		}
+		return val, nil
 	}
 	return nil, nil
+}
+
+// decodeInteger decodes the byte slice as a singed 64bit integer. The
+// ':' prefix is assumed to be already consumed.
+func decodeInteger(b []byte) (val Integer, n int, err error) {
+	var cr bool
+	var sign Integer = 1
+
+loop:
+	for i := 0; i < len(b); i++ {
+		ch := b[i]
+		n++
+
+		switch ch {
+		case '\r':
+			cr = true
+			break loop
+
+		case '\n':
+			// Will return with ErrMissingCRLF
+			break loop
+
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			val = val*10 + Integer(ch-'0')
+
+		case '-':
+			if i == 0 {
+				sign = -1
+				continue
+			}
+			fallthrough
+		default:
+			return 0, n, ErrInvalidInteger
+		}
+	}
+
+	if !cr {
+		return 0, n, ErrMissingCRLF
+	}
+	return sign * val, n, nil
 }
 
 // decodeSimpleString decodes the byte slice as a SimpleString. The
--- resp/parser_test.go
+++ resp/parser_test.go
@@ -6,8 +6,9 @@ import (
 )
 
 var simpleStrings = []struct {
-	in, out []byte
-	err     error
+	in  []byte
+	out interface{}
+	err error
 }{
 	0: {[]byte{'+', '\r', '\n'}, []byte{}, nil},
 	1: {[]byte{'+', 'a', '\r', '\n'}, []byte{'a'}, nil},
@@ -18,8 +19,9 @@ var simpleStrings = []struct {
 }
 
 var errs = []struct {
-	in, out []byte
-	err     error
+	in  []byte
+	out interface{}
+	err error
 }{
 	0: {[]byte{'-', '\r', '\n'}, []byte{}, nil},
 	1: {[]byte{'-', 'a', '\r', '\n'}, []byte{'a'}, nil},
@@ -27,6 +29,21 @@ var errs = []struct {
 	3: {[]byte("-ceci n'est pas un string\r\n"), []byte("ceci n'est pas un string"), nil},
 	4: {[]byte("-ceci n'est pas un string"), nil, ErrMissingCRLF},
 	5: {[]byte("-"), nil, ErrMissingCRLF},
+}
+
+var integers = []struct {
+	in  []byte
+	out interface{}
+	err error
+}{
+	0: {[]byte(":\r\n"), Integer(0), nil},
+	1: {[]byte(":1\r\n"), Integer(1), nil},
+	2: {[]byte(":123\r\n"), Integer(123), nil},
+	3: {[]byte(":123\n"), nil, ErrMissingCRLF},
+	4: {[]byte(":123a\r\n"), nil, ErrInvalidInteger},
+	5: {[]byte(":-123\r\n"), Integer(-123), nil},
+	6: {[]byte(":123"), nil, ErrMissingCRLF},
+	7: {[]byte(":-1-3\r\n"), nil, ErrInvalidInteger},
 }
 
 func TestSimpleString(t *testing.T) {
@@ -40,10 +57,8 @@ func TestSimpleString(t *testing.T) {
 		}
 		if ss, ok := got.(SimpleString); !ok {
 			t.Errorf("%d: expected a simple string, got %T", i, got)
-		} else {
-			if bytes.Compare(ss, c.out) != 0 {
-				t.Errorf("%d: expected output %X (%q), got %X (%q)", i, c.out, string(c.out), ss, string(ss))
-			}
+		} else if bytes.Compare(ss, c.out.([]byte)) != 0 {
+			t.Errorf("%d: expected output %X (%q), got %X (%q)", i, c.out, string(c.out.([]byte)), ss, string(ss))
 		}
 	}
 }
@@ -57,12 +72,27 @@ func TestError(t *testing.T) {
 		if got == nil && c.out == nil {
 			continue
 		}
-		if ss, ok := got.(Error); !ok {
+		if er, ok := got.(Error); !ok {
 			t.Errorf("%d: expected an error, got %T", i, got)
-		} else {
-			if bytes.Compare(ss, c.out) != 0 {
-				t.Errorf("%d: expected output %X (%q), got %X (%q)", i, c.out, string(c.out), ss, string(ss))
-			}
+		} else if bytes.Compare(er, c.out.([]byte)) != 0 {
+			t.Errorf("%d: expected output %X (%q), got %X (%q)", i, c.out, string(c.out.([]byte)), er, string(er))
+		}
+	}
+}
+
+func TestInteger(t *testing.T) {
+	for i, c := range integers {
+		got, err := decodePayload(c.in, false)
+		if err != c.err {
+			t.Errorf("%d: expected error %v, got %v", i, c.err, err)
+		}
+		if got == nil && c.out == nil {
+			continue
+		}
+		if ii, ok := got.(Integer); !ok {
+			t.Errorf("%d: expected an integer, got %T", i, got)
+		} else if c.out.(Integer) != ii {
+			t.Errorf("%d: expected output %d, got %d", i, c.out, ii)
 		}
 	}
 }
