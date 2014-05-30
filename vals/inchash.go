81fd2961d864904b24f6dc602b00d132e378443f
Martin Angers <martin.n.angers@gmail.com>
2014-05-30 15:43:15 -0400

    vals package doc

--- vals/inchash.go
+++ vals/inchash.go
@@ -2,25 +2,34 @@ package vals
 
 import "strconv"
 
+// IncHash defines the methods required for an incrementable hash value.
+// A IncHash is a Hash with additional methods to increment numeric values.
 type IncHash interface {
 	Hash
 	HIncrBy(string, int64) (int64, bool)
 	HIncrByFloat(string, float64) (string, bool)
 }
 
+// Static type check to validate that *incHash implements IncHash.
 var _ IncHash = (*incHash)(nil)
 
+// incHash implements a IncHash.
 type incHash struct {
 	Hash
 	// TODO : May hold the parsed integer value eventually
 }
 
+// NewIncHash creates a new IncHash.
 func NewIncHash() IncHash {
 	return &incHash{
 		NewHash(),
 	}
 }
 
+// HIncrBy increments the value of field by inc. It creates the field
+// and sets it at 0 before incrementing if field does not exist in the hash.
+// It returns false as second return value if it could not perform the
+// increment because the current value is not numeric.
 func (ih *incHash) HIncrBy(field string, inc int64) (int64, bool) {
 	val, ok := ih.HGet(field)
 	if !ok {
@@ -36,6 +45,10 @@ func (ih *incHash) HIncrBy(field string, inc int64) (int64, bool) {
 	return v, true
 }
 
+// HIncrByFloat increments the value of field by inc. It creates the field
+// and sets it at 0 before incrementing if field does not exist in the hash.
+// It returns false as second return value if it could not perform the
+// increment because the current value is not numeric.
 func (ih *incHash) HIncrByFloat(field string, inc float64) (string, bool) {
 	val, ok := ih.HGet(field)
 	if !ok {
